32b792462dba153af55ceb93119b61ef9092199f
Jason Ancrum <jason.ancrum00@delta.com>
2022-06-28 13:30:33 -0400

    add the appropriate endpoint funcs

--- go.mod
+++ go.mod
@@ -3,3 +3,55 @@ module example.com/m
 go 1.18
 
 require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/adamzy/cedar-go v0.0.0-20170805034717-80a9c64b256d // indirect
+	github.com/astaxie/beego v1.12.3 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/casbin/casnode v1.3.0 // indirect
+	github.com/casbin/google-groups-crawler v0.1.3 // indirect
+	github.com/casdoor/casdoor-go-sdk v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/chris-ramon/douceur v0.2.0 // indirect
+	github.com/chromedp/cdproto v0.0.0-20210323015217-0942afbea50e // indirect
+	github.com/chromedp/chromedp v0.6.10 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.0.4 // indirect
+	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20210408062403-ad838ccf8cdd // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/huichen/sego v0.0.0-20180617034105-3f3c8a8cfacc // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.5 // indirect
+	github.com/mileusna/crontab v1.0.1 // indirect
+	github.com/mozillazg/go-slugify v0.2.0 // indirect
+	github.com/mozillazg/go-unidecode v0.1.1 // indirect
+	github.com/mreiferson/go-httpclient v0.0.0-20201222173833-5e475fde3a4d // indirect
+	github.com/mrexodia/wray v0.0.0-20160318003008-78a2c1f284ff // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/sromku/go-gitter v0.0.0-20170828210750-70f7030a94a6 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	xorm.io/builder v0.3.6 // indirect
+	xorm.io/core v0.7.2 // indirect
+	xorm.io/xorm v0.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+
+	//	"io/utils"
 	"math/rand"
 	"net/http"
 	"os"
@@ -43,6 +45,10 @@ type PatientRecord struct {
 }
 
 var PatientRecords []PatientRecord
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	// A very simple health check.
@@ -122,53 +128,99 @@ func PatientsEdit(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 	pr.patient.ID = id
-
+	json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
 	respondWithJSON(w, http.StatusOK, pr)
 }
 
-// Get ALL Patients
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the
+	// directory we just made
+	staticFileDirectory := http.Dir("./assets/")
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/assets/" prefix when looking for files.
+	// For example, if we type "/assets/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for
+	// "./assets/assets/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	return r
+}
+
+func getPatientsHandler(w http.ResponseWriter, r *http.Request) {
+	//Transform our patientrecord to json
+	patientRecBytes, err := json.Marshal(PatientRecords)
+
+	// If there is an error, print it to the console, and return a server
+	// error response to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// If all goes well, write the JSON list of birds to the response
+	w.Write(patientRecBytes)
+}
+
+func createPatientsHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a new instance of Patient Record
+	pat := PatientRecord{}
+
+	// We send all our data as HTML form data
+	// the `ParseForm` method of the request, parses the
+	// form values
+	err := r.ParseForm()
+
+	// In case of any error, we respond with an error to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Get the information about the patient from the form info
+	pat.patient.FirstName = r.Form.Get("species")
+	pat.patient.LastName = r.Form.Get("description")
+
+	// Append our existing list of birds with a new entry
+	PatientRecords = append(PatientRecords, pat)
+
+	//Finally, we redirect the user to the original HTMl page
+	// (located at `/assets/`), using the http libraries `Redirect` method
+	http.Redirect(w, r, "/assets/", http.StatusFound)
+}
+
+// Get ALL Patients 1st iteration
 func PatientsAll(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	json.NewEncoder(w).Encode(PatientRecords)
-}
+	//json.NewEncoder(w).Encode(PatientRecords)
+	json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
 
-// Helper function for errors rendering
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, map[string]string{"error": message})
-}
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
 
-// Server initialization and Startup
+	// Setup the decoder and call the DisallowUnknownFields() method on it.
+	// This will cause Decode() to return a "json: unknown field ..." error
+	// if it encounters any extra unexpected fields in the JSON. Strictly
+	// speaking, it returns an error for "keys which do not match any
+	// non-ignored, exported fields in the destination".
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
 
-func serverStart() {
-	// Construct the routing site endpoints
-	router := mux.NewRouter()
-	router.HandleFunc("/", PatientsAll).Methods("GET")
-	router.HandleFunc("/patients", PatientsAll).Methods("GET")
-	router.HandleFunc("/patients/{id:[0-9]+}", PatientsFind).Methods("GET")
-	router.HandleFunc("/patients", PatientsCreate).Methods("POST")
-	router.HandleFunc("/patients/{id:[0-9]+}", PatientsEdit).Methods("PUT")
-	router.HandleFunc("/patients/{id:[0-9]+}", PatientsDelete).Methods("DELETE")
-	router.HandleFunc("/health", HealthCheckHandler)
-
-	http.Handle("/", router)
-	// TODO need more graceful startup and server shutdown
-
-	// Start server
-	port := ":8888"
-	fmt.Println("\nListening and serving up content on port " + port)
-	http.ListenAndServe(port, router)
-}
-
-func main() {
+	err := dec.Decode(&PatientRecords)
+	if err != io.EOF {
+		msg := "Request body must only contain a single JSON object"
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
 
 	// Initializing the first and second patient objects
-	p1 := &PatientRecord{
+	p1 := PatientRecord{
 		patient: Patient{
 			ID:            12345,
 			FirstName:     "Jane",
@@ -191,7 +243,129 @@ func main() {
 		},
 	}
 
-	p2 := &PatientRecord{
+	p2 := PatientRecord{
+		patient: Patient{
+			ID:            67890,
+			FirstName:     "John",
+			LastName:      "Doe",
+			StreetAddress: "5678 Some Patient Drive,  Some City USA",
+			State:         "GA",
+			Zip:           67890,
+			Telephone:     4040000000,
+		},
+		demographic:      "African American",
+		medHistory:       []string{"Some medical history 1", "Some medical history 2", "Some medical history 3"},
+		labResults:       []string{"Some lab results 1", "Some lab results 2", "Some lab results 3"},
+		mentalHealth:     "Some mental health assessment",
+		insuranceCarrier: "XYZ GlobalLife",
+		billing: Billing{
+			primaryCC:   4444222266669999, //TODO to encode this cleartext field of CCnumber
+			billingAddr: "5678 Some Client Drive, Some City USA, 67890",
+			owing:       true,
+			balance:     850.00,
+		},
+	}
+
+	json.NewEncoder(w).Encode(p1.patient.FirstName)
+	json.NewEncoder(w).Encode(p2.patient.LastName)
+
+}
+
+// Helper function for errors rendering
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// Print the entire PatientRecords collection currently in memory
+func PrintAllStruct(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var printSample PatientRecord
+	printSample = PatientRecord{
+		patient: Patient{
+			ID:            0000000,
+			FirstName:     "Hello from PrintAllStruct (see fields)",
+			LastName:      "Doe",
+			StreetAddress: "1234 Some Printer stuff,  Some City USA",
+			State:         "GA",
+			Zip:           12345,
+			Telephone:     7700000000,
+		},
+		demographic:      "We're all the same",
+		medHistory:       []string{"Some medical history 1", "Some medical history 2", "Some medical history 3"},
+		labResults:       []string{"Some lab results 1", "Some lab results 2", "Some lab results 3"},
+		mentalHealth:     "Some mental health assessment",
+		insuranceCarrier: "XYZ UnitedHealth",
+		billing: Billing{
+			primaryCC:   4444000011115555, //TODO to encode this cleartext field of CCnumber
+			billingAddr: "1234 Some Client Drive, Some City USA, 12345",
+			owing:       false,
+			balance:     000.00,
+		},
+	}
+
+	io.WriteString(w, printSample.patient.FirstName)
+}
+
+// Server initialization and Startup
+
+func serverStart() {
+	// Construct the routing site endpoints
+	router := mux.NewRouter()
+	router.HandleFunc("/", getPatientsHandler).Methods("GET")
+	router.HandleFunc("/patients", getPatientsHandler).Methods("GET")
+	router.HandleFunc("/patients/{id:[0-9]+}", PatientsFind).Methods("GET")
+	router.HandleFunc("/create", getPatientsHandler).Methods("GET")
+	router.HandleFunc("/create", createPatientsHandler).Methods("POST")
+	router.HandleFunc("/patients/edit/{id:[0-9]+}", PatientsEdit).Methods("PUT")
+	router.HandleFunc("/patients/delete/{id:[0-9]+}", PatientsDelete).Methods("DELETE")
+	router.HandleFunc("/print", PrintAllStruct).Methods("GET")
+	router.HandleFunc("/health", HealthCheckHandler)
+	router.HandleFunc("/hello", handler).Methods("GET")
+
+	http.Handle("/", router)
+	// TODO need more graceful startup and server shutdown
+
+	// Start server
+	port := ":8888"
+	fmt.Println("\nListening and serving up content on port " + port)
+	http.ListenAndServe(port, router)
+}
+
+func main() {
+
+	// Initializing the first and second patient objects
+	p1 := PatientRecord{
+		patient: Patient{
+			ID:            12345,
+			FirstName:     "Jane",
+			LastName:      "Doe",
+			StreetAddress: "1234 Some Patient Drive,  Some City USA",
+			State:         "GA",
+			Zip:           12345,
+			Telephone:     7700000000,
+		},
+		demographic:      "Pacific Islander",
+		medHistory:       []string{"Some medical history 1", "Some medical history 2", "Some medical history 3"},
+		labResults:       []string{"Some lab results 1", "Some lab results 2", "Some lab results 3"},
+		mentalHealth:     "Some mental health assessment",
+		insuranceCarrier: "XYZ UnitedHealth",
+		billing: Billing{
+			primaryCC:   4444000011115555, //TODO to encode this cleartext field of CCnumber
+			billingAddr: "1234 Some Client Drive, Some City USA, 12345",
+			owing:       false,
+			balance:     000.00,
+		},
+	}
+
+	p2 := PatientRecord{
 		patient: Patient{
 			ID:            67890,
 			FirstName:     "John",
