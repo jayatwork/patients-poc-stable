23b1460af3a24be3fdce4af47eed4ec4826b5355
Jason Ancrum <jason.ancrum00@delta.com>
2022-06-30 10:24:12 -0400

    repaired the existing cruds only ui pending

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math/rand"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -42,27 +43,24 @@ type PatientRecord struct {
 	billing          Billing
 }
 
-var PatientRecords []PatientRecord
+var Patients []Patient // Globally scoped Patient collection to be consulted throughout
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	// A very simple health check.
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-
-	// In the future we could report back on the status of our DB, or our cache
-	// (e.g. Redis) by performing a simple PING, and include them in the response.
+	log.Println("Endpoint Hit: /health")
 	io.WriteString(w, `{"alive": true}`)
 }
 
 // Create new Patient Record
-func PatientsCreate(w http.ResponseWriter, r *http.Request) {
-	var pr PatientRecord                        //initialize with struct
-	json.NewDecoder(r.Body).Decode(&pr)         //passing JSON object to struct
-	pr.patient.ID = rand.Intn(100)              //Generate random ID
-	PatientRecords = append(PatientRecords, pr) //save user into all users in memory
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode("user Created")
+func patientsCreate(w http.ResponseWriter, r *http.Request) {
+	var pat Patient //initialize with struct
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &pat)
+	Patients = append(Patients, pat)
+	json.NewEncoder(w).Encode(Patients)
+	log.Println("Endpoint Hit: /create")
 }
 
 // Return a single patient record by patient.ID
@@ -71,17 +69,18 @@ func PatientsFind(w http.ResponseWriter, r *http.Request) {
 	key := vars["ID"]
 
 	// Loop over all of our existing PatientRecords
-	for _, pr := range PatientRecords {
+	for _, pat := range Patients {
 		id, err := strconv.Atoi(key)
 		if err != nil {
 			// handle error
 			fmt.Println(err)
 			os.Exit(2)
 		}
-		if pr.patient.ID == id {
-			json.NewEncoder(w).Encode(pr.patient.ID)
+		if pat.ID == id {
+			json.NewEncoder(w).Encode(pat.ID)
 		}
 	}
+	log.Println("Endpoint Hit: /find")
 }
 
 // Delete a patients record entry
@@ -89,20 +88,20 @@ func PatientsDelete(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	for index, pr := range PatientRecords {
+	for index, pat := range Patients {
 		id, err := strconv.Atoi(params["id"])
 		if err != nil {
 			// handle error
 			fmt.Println(err)
 			os.Exit(2)
 		}
-		if pr.patient.ID == id {
-			PatientRecords = append(PatientRecords[:index], PatientRecords[index+1:]...)
+		if pat.ID == id {
+			Patients = append(Patients[:index], Patients[index+1:]...)
 			json.NewEncoder(w).Encode("Successfully deleted !!!")
 			return
 		}
 	}
-
+	log.Println("Endpoint Hit: /delete")
 	w.WriteHeader(http.StatusNotFound)
 	json.NewEncoder(w).Encode("User Not Found")
 }
@@ -114,22 +113,23 @@ func PatientsEdit(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
 		return
 	}
-	var pr PatientRecord
+	var pat Patient
 	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&pr); err != nil {
+	if err := decoder.Decode(&pat); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
 		return
 	}
 	defer r.Body.Close()
-	pr.patient.ID = id
+	pat.ID = id
 
-	respondWithJSON(w, http.StatusOK, pr)
+	respondWithJSON(w, http.StatusOK, pat)
 }
 
 // Get ALL Patients
 func PatientsAll(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: / or /patients")
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	json.NewEncoder(w).Encode(PatientRecords)
+	json.NewEncoder(w).Encode(Patients)
 }
 
 // Helper function for errors rendering
@@ -143,20 +143,40 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(response)
 }
 
+//jsonHandler returns http response in JSON format.
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//Construct an initiat Patient object to render
+	pat := Patient{
+		ID:            67890,
+		FirstName:     "John",
+		LastName:      "Doe",
+		StreetAddress: "5678 Some Patient Drive,  Some City USA",
+		State:         "GA",
+		Zip:           67890,
+		Telephone:     4040000000,
+	}
+	log.Println("Endpoint Hit: /json")
+	Patients = append(Patients, pat) //save initial patient in memory
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(Patients)
+	json.NewEncoder(w).Encode(pat)
+}
+
 // Server initialization and Startup
 
 func serverStart() {
 	// Construct the routing site endpoints
 	router := mux.NewRouter()
-	router.HandleFunc("/", PatientsAll).Methods("GET")
-	router.HandleFunc("/patients", PatientsAll).Methods("GET")
-	router.HandleFunc("/patients/{id:[0-9]+}", PatientsFind).Methods("GET")
-	router.HandleFunc("/patients", PatientsCreate).Methods("POST")
-	router.HandleFunc("/patients/{id:[0-9]+}", PatientsEdit).Methods("PUT")
-	router.HandleFunc("/patients/{id:[0-9]+}", PatientsDelete).Methods("DELETE")
+	router.HandleFunc("/json", jsonHandler)
+	router.HandleFunc("/", PatientsAll)
+	router.HandleFunc("/patients", PatientsAll)
+	router.HandleFunc("/find/{id}", PatientsFind)
+	router.HandleFunc("/create", patientsCreate).Methods("POST")
+	router.HandleFunc("/edit/{id}", PatientsEdit)
+	router.HandleFunc("/delete/{id}", PatientsDelete).Methods("DELETE")
 	router.HandleFunc("/health", HealthCheckHandler)
 
-	http.Handle("/", router)
 	// TODO need more graceful startup and server shutdown
 
 	// Start server
@@ -166,57 +186,5 @@ func serverStart() {
 }
 
 func main() {
-
-	// Initializing the first and second patient objects
-	p1 := &PatientRecord{
-		patient: Patient{
-			ID:            12345,
-			FirstName:     "Jane",
-			LastName:      "Doe",
-			StreetAddress: "1234 Some Patient Drive,  Some City USA",
-			State:         "GA",
-			Zip:           12345,
-			Telephone:     7700000000,
-		},
-		demographic:      "Pacific Islander",
-		medHistory:       []string{"Some medical history 1", "Some medical history 2", "Some medical history 3"},
-		labResults:       []string{"Some lab results 1", "Some lab results 2", "Some lab results 3"},
-		mentalHealth:     "Some mental health assessment",
-		insuranceCarrier: "XYZ UnitedHealth",
-		billing: Billing{
-			primaryCC:   4444000011115555, //TODO to encode this cleartext field of CCnumber
-			billingAddr: "1234 Some Client Drive, Some City USA, 12345",
-			owing:       false,
-			balance:     000.00,
-		},
-	}
-
-	p2 := &PatientRecord{
-		patient: Patient{
-			ID:            67890,
-			FirstName:     "John",
-			LastName:      "Doe",
-			StreetAddress: "5678 Some Patient Drive,  Some City USA",
-			State:         "GA",
-			Zip:           67890,
-			Telephone:     4040000000,
-		},
-		demographic:      "African American",
-		medHistory:       []string{"Some medical history 1", "Some medical history 2", "Some medical history 3"},
-		labResults:       []string{"Some lab results 1", "Some lab results 2", "Some lab results 3"},
-		mentalHealth:     "Some mental health assessment",
-		insuranceCarrier: "XYZ GlobalLife",
-		billing: Billing{
-			primaryCC:   4444222266669999, //TODO to encode this cleartext field of CCnumber
-			billingAddr: "5678 Some Client Drive, Some City USA, 67890",
-			owing:       true,
-			balance:     850.00,
-		},
-	}
-
-	fmt.Println(p1, "\n", p2)         //Observe initial data structure
-	fmt.Println(p1.billing.primaryCC) //Eventually check if known cleartext fields are obfuscated
-
 	serverStart()
-
 }
